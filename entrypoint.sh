68b41ccf1c6e4c265b91e1ce1269783ce7ce609d
Michel Wilhelm <michelwilhelm@gmail.com>
2025-01-28 11:14:26 -0300

    Fix directory privilege

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,9 @@ if [ -n "${DEV_TUNNEL_SSH_PASSWORD}" ]; then
     echo "dev:${DEV_TUNNEL_SSH_PASSWORD}" | chpasswd
 fi
 
+# Create the directory for privilege separation if it doesn't exist
+mkdir -p /run/sshd
+
 # Start SSH service
 echo "Starting SSH service"
 /usr/sbin/sshd -D
